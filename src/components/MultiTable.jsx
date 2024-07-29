9188ec76eaa7179dec4afa51ca743e4ae0c5c41b
userJFD <joelfregno@outlook.com>
2024-07-29 19:27:11 +0200

    MultiCalc Logic implemented

--- src/components/MultiTable.jsx
+++ src/components/MultiTable.jsx
@@ -17,7 +17,7 @@ const PRODUCT_DATA_URL = '/data/products.json';
 const VAT_RATES = {
   "Switzerland": {
     "food": 2.5,
-    "non-food": 7.7
+    "non-food": 8.1
   },
   "Germany": {
     "food": 7,
@@ -26,19 +26,39 @@ const VAT_RATES = {
 };
 
 function MultiTable() {
+  const defaultVatRate = VAT_RATES["Switzerland"]["non-food"];
+
   const [customers, setCustomers] = useState([]);
   const [customerName, setCustomerName] = useState("");
   const [customerID, setCustomerID] = useState("");
+  const [dealRange, setDealRange] = useState("");
   const [products, setProducts] = useState([]);
   const [productType, setProductType] = useState("");
   const [category, setCategory] = useState("");
-  const [dealRange, setDealRange] = useState("");
   const [productDescription, setProductDescription] = useState("");
   const [supplierDescription, setSupplierDescription] = useState("");
   const [units, setUnits] = useState(0);
-  const [purchaseAmount, setPurchaseAmount] = useState(0);
-  const [vatRate, setVatRate] = useState(VAT_RATES["Switzerland"]["food"]);
-  const [marginPercent, setMarginPercent] = useState(0); 
+
+
+  const [purchaseAmount, setPurchaseAmount] = useState('0.00');
+  const [deliveryAmount, setDeliveryAmount] = useState('0.00');
+  const [kickbacksPercent, setKickbacksPercent] = useState('0.00');
+  const [vatRate, setVatRate] = useState(defaultVatRate);
+  const [marginPercent, setMarginPercent] = useState('0.00');
+
+
+  const purchaseAmountNumber = parseFloat(purchaseAmount) || 0;
+  const deliveryAmountNumber = parseFloat(deliveryAmount) || 0;
+  const kickbacksPercentNumber = parseFloat(kickbacksPercent) || 0;
+  const vatPercentNumber = vatRate ? parseFloat(vatRate) : 0;
+  const marginPercentNumber = parseFloat(marginPercent) || 0;
+  
+  const kickbacksAmount = (purchaseAmountNumber * kickbacksPercentNumber) / 100;
+  const minimumSellingPrice = (purchaseAmountNumber + deliveryAmountNumber) - kickbacksAmount;
+  const marginAmount = (minimumSellingPrice * marginPercentNumber) / (100 - marginPercentNumber);
+  const sellingPriceExclVat = minimumSellingPrice + marginAmount;
+  const vatAmount = (sellingPriceExclVat * vatPercentNumber) / 100;
+  const sellingPriceInclVat = sellingPriceExclVat + vatAmount;
 
   // Generate options array based on VAT_RATES and flag URLs
   const flagUrls = {
@@ -81,8 +101,12 @@ function MultiTable() {
       return (purchaseAmount * marginPercent) / 100;
     }
     return 0;
-  };
+  }; 
 
+  const handleChange = (selectedOption) => {
+    setVatRate(selectedOption.value); // Set the VAT rate directly
+  };
+  
   // Fetch customer data from API
   useEffect(() => {
     fetch(CUSTOMER_DATA_URL)
@@ -105,7 +129,7 @@ function MultiTable() {
 
   return (
     <div className="table-wrapper">
-      <table className="fl-table">
+      <table className="fl-tabletwo">
         <thead>
           <tr>
             <th>Product Description</th>
@@ -113,13 +137,16 @@ function MultiTable() {
             <th>PT</th>
             <th>CAT</th>
             <th>Supplier</th>
-            <th>Units</th>
-            <th>EK (excl.VAT)</th>
-            <th>VAT %</th>
-            <th>Margin %</th>
-            <th>Margin per Unit</th>
-            <th>VK (excl.VAT)</th>
-            <th>VK (incl.VAT)</th>
+            <th className="numberCol">Units</th>
+            <th className="numberCol">EK <p className='thSubtitle'>(excl.VAT)</p> </th>
+            <th className="numberCol">Delivery<br/> Surcharge</th>
+            <th className="numberCol">Kickbacks <p className='thSubtitle'>in %</p></th>
+            <th className="vatCol">VAT <p className='thSubtitle'> in %</p></th>
+            <th>Recommended <br/> Margin</th>
+            <th className="numberCol">Margin <p className='thSubtitle'>in %</p></th>
+            <th>Margin <p className='thSubtitle'> per Unit</p></th>
+            <th>VK <p className='thSubtitle'>(excl.VAT)</p></th>
+            <th>VK <p className='thSubtitle'>(incl.VAT)</p></th>
           </tr>
         </thead>
         <tbody>
@@ -158,6 +185,7 @@ function MultiTable() {
             </td>
             <td>
               <input
+                className="numberCol"
                 type="number"
                 step="0.01"
                 value={purchaseAmount}
@@ -166,9 +194,30 @@ function MultiTable() {
               />
             </td>
             <td>
+              <input
+                className="numberCol"
+                type="number"
+                step="0.01"
+                value={deliveryAmount}
+                onChange={(e) => {
+                  setDeliveryAmount(e.target.value);
+                }}
+              />
+            </td>
+            <td>
+              <input
+                className="numberCol"
+                type="number"
+                step="0.01"
+                value={kickbacksPercent}
+                onChange={(e) => setKickbacksPercent(e.target.value)}
+              />
+            </td>
+            <td>
               <Select
+                className="vatCol"
                 value={options.find(option => option.value === vatRate)}
-                onChange={handleVatRateChange}
+                onChange={handleChange}
                 options={options}
                 getOptionLabel={(option) => (
                   <div style={{ display: 'flex', alignItems: 'center' }}>
@@ -180,16 +229,22 @@ function MultiTable() {
               />
             </td>
             <td>
+              <p>soon i will be good</p>
+            </td>
+            <td>
               <input
+                className="numberCol"
                 type="number"
                 step="0.01"
                 value={marginPercent}
-                onChange={(e) => setMarginPercent(parseFloat(e.target.value) || 0)}
+                onChange={(e) => setMarginPercent(e.target.value)}
               />
             </td>
             <td>
               {calculateMarginPerUnit().toFixed(2)}
             </td>
+            <td>{sellingPriceExclVat.toFixed(2)}</td>
+            <td className='sellingpricetotal'>{sellingPriceInclVat.toFixed(2)}</td>
           </tr>
         </tbody>
       </table>
@@ -200,3 +255,27 @@ function MultiTable() {
 export default MultiTable;
 
 
+/* <td>
+<Select
+  value={options.find(option => option.value === vatRate)}
+  onChange={handleVatRateChange}
+  options={options}
+  getOptionLabel={(option) => (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={option.flag} alt={option.label} style={{ width: '20px', height: '15px', marginRight: '8px' }} />
+      {option.label}
+    </div>
+  )}
+  components={{ SingleValue: customSingleValue, Option: customOption }}
+/>
+</td> */
+
+
+/* <td>
+              <input
+                type="number"
+                step="0.01"
+                value={marginPercent}
+                onChange={(e) => setMarginPercent(parseFloat(e.target.value) || 0)}
+              />
+            </td> */
